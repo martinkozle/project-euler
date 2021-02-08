19d4feae893528ee56e120f2828d92f0ba1bca7b
martinkozle <martinkozle@yahoo.com>
2021-02-08 10:14:00 +0100

    Solution to problem 357 and created requirements.txt for tqdm

---
+++ p357.py
@@ -0,0 +1,25 @@
+# 1739023853137
+from eulerlib import proper_divisors, prime_generator
+
+
+from tqdm import tqdm
+
+
+def main():
+    # this one took an hour and 50 minutes to compute
+    total = 100000000
+    primes_set = set(tqdm(prime_generator(limit=total), total=total))
+    s = 0
+    for i in tqdm(primes_set):
+        flag = True
+        for d in proper_divisors(i - 1):
+            if d + (i - 1) // d not in primes_set:
+                flag = False
+                break
+        if flag:
+            s += i - 1
+    print(s)
+
+
+if __name__ == '__main__':
+    main()
